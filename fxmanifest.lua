8fce2bb50204eeced3908fadceb0b42416f2c154
BL SCRIPTS <jordanhitchcock04@gmail.com>
2025-11-07 02:43:44 +1030

    Updated fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,28 +3,29 @@ game 'gta5'
 
 author 'BL SCRIPTS'
 description 'Vehicle Lock System (QB/ESX/Standalone)'
-version '1.0.1'
+version '1.0.2'
 
 lua54 'yes'
 
 shared_script '@ox_lib/init.lua'
 
 shared_scripts {
-    'config/config.lua',  
-    'locales/en.lua',      
-    '@bl_lib/lib.lua',     
+    'config/config.lua',
+    'locales/en.lua',
+    '@bl_lib/lib.lua'
 }
 
 client_scripts {
-    'client/main.lua',    
-    'client/target.lua',   
+    'client/main.lua',
+    'client/target.lua'
 }
 
 server_scripts {
-    '@oxmysql/lib/MySQL.lua',  
-    'server/main.lua',           
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
 }
 
 dependencies {
-    'bl_lib',           
+    'bl_lib',
+    'InteractSound'
 }
