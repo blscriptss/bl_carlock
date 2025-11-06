40abf15dd049acb374ea353fef270a5d432c0c6e
BL SCRIPTS <jordanhitchcock04@gmail.com>
2025-11-07 02:28:12 +1030

    Updated en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,6 +1,5 @@
 Locales = {}
 
--- Vehicle Lock/Unlock
 Locales['vehicle_locked'] = "Vehicle locked."
 Locales['vehicle_unlocked'] = "Vehicle unlocked."
 Locales['not_driver'] = "You must be the driver to do that."
@@ -11,7 +10,6 @@ Locales['key_received'] = "You have received keys to the vehicle."
 Locales['key_already_has'] = "That player already has keys to this vehicle."
 Locales['no_keys_to_give'] = "You don't have keys to give for this vehicle."
 
--- Vehicle Lock Failure (Edge cases)
 Locales['vehicle_locked_fail'] = "Failed to lock the vehicle. Please try again."
 Locales['vehicle_unlocked_fail'] = "Failed to unlock the vehicle. Please try again."
 Locales['vehicle_no_key'] = "You don't have a key for this vehicle."
@@ -19,23 +17,19 @@ Locales['vehicle_no_permission'] = "You don't have permission to unlock this veh
 Locales['vehicle_already_locked'] = "This vehicle is already locked."
 Locales['vehicle_already_unlocked'] = "This vehicle is already unlocked."
 
--- Target Labels
 Locales['target_lock'] = "Lock Vehicle"
 Locales['target_unlock'] = "Unlock Vehicle"
 Locales['target_give_key'] = "Give Vehicle Key"
 
--- Key Interaction
 Locales['key_taken_from_vehicle'] = "Key taken from vehicle."
 Locales['key_stolen'] = "Key has been stolen from your vehicle!"
 Locales['key_given_to_player'] = "Key successfully given to player."
 
--- General Feedback
 Locales['no_players_nearby'] = "No players nearby."
 Locales['player_has_no_keys'] = "This player doesn't have keys for this vehicle."
 Locales['player_received_key'] = "You have received the keys to this vehicle."
 Locales['player_gave_key'] = "You have given the keys to this vehicle."
 
--- Debugging (optional for debugging purposes in development)
 Locales['debug_vehicle_locked'] = "Vehicle successfully locked."
 Locales['debug_vehicle_unlocked'] = "Vehicle successfully unlocked."
 
