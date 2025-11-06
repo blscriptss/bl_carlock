64453a306922423962659edf119f8b0ce3a7b129
BL SCRIPTS <jordanhitchcock04@gmail.com>
2025-11-07 02:28:56 +1030

    Updated fr.lua

--- locales/fr.lua
+++ locales/fr.lua
@@ -1,41 +1,34 @@
 Locales = {}
 
--- Vehicle Lock/Unlock
-Locales['vehicle_locked'] = "Véhicule verrouillé."
-Locales['vehicle_unlocked'] = "Véhicule déverrouillé."
-Locales['not_driver'] = "Vous devez être le conducteur pour faire cela."
-Locales['no_vehicle_nearby'] = "Aucun véhicule à proximité."
-Locales['not_your_vehicle'] = "Vous n'avez pas les clés de ce véhicule."
-Locales['key_given'] = "Vous avez donné les clés du véhicule."
-Locales['key_received'] = "Vous avez reçu les clés du véhicule."
-Locales['key_already_has'] = "Ce joueur a déjà les clés de ce véhicule."
-Locales['no_keys_to_give'] = "Vous n'avez pas de clés à donner pour ce véhicule."
+Locales['vehicle_locked'] = "VÃ©hicule verrouillÃ©."
+Locales['vehicle_unlocked'] = "ðŸ”“ VÃ©hicule dÃ©verrouillÃ©."
+Locales['not_driver'] = "Vous devez Ãªtre le conducteur pour faire cela."
+Locales['no_vehicle_nearby'] = "Aucun vÃ©hicule Ã  proximitÃ©."
+Locales['not_your_vehicle'] = "Vous n'avez pas les clÃ©s de ce vÃ©hicule."
+Locales['key_given'] = "Vous avez donnÃ© les clÃ©s du vÃ©hicule."
+Locales['key_received'] = "Vous avez reÃ§u les clÃ©s du vÃ©hicule."
+Locales['key_already_has'] = "Ce joueur a dÃ©jÃ  les clÃ©s de ce vÃ©hicule."
+Locales['no_keys_to_give'] = "Vous n'avez pas de clÃ©s Ã  donner pour ce vÃ©hicule."
 
--- Vehicle Lock Failure (Edge cases)
-Locales['vehicle_locked_fail'] = "Échec du verrouillage du véhicule. Veuillez réessayer."
-Locales['vehicle_unlocked_fail'] = "Échec du déverrouillage du véhicule. Veuillez réessayer."
-Locales['vehicle_no_key'] = "Vous n'avez pas la clé de ce véhicule."
-Locales['vehicle_no_permission'] = "Vous n'avez pas la permission de déverrouiller ce véhicule."
-Locales['vehicle_already_locked'] = "Ce véhicule est déjà verrouillé."
-Locales['vehicle_already_unlocked'] = "Ce véhicule est déjà déverrouillé."
+Locales['vehicle_locked_fail'] = "Ã‰chec du verrouillage du vÃ©hicule. Veuillez rÃ©essayer."
+Locales['vehicle_unlocked_fail'] = "Ã‰chec du dÃ©verrouillage du vÃ©hicule. Veuillez rÃ©essayer."
+Locales['vehicle_no_key'] = "Vous n'avez pas la clÃ© de ce vÃ©hicule."
+Locales['vehicle_no_permission'] = "Vous n'avez pas la permission de dÃ©verrouiller ce vÃ©hicule."
+Locales['vehicle_already_locked'] = "Ce vÃ©hicule est dÃ©jÃ  verrouillÃ©."
+Locales['vehicle_already_unlocked'] = "Ce vÃ©hicule est dÃ©jÃ  dÃ©verrouillÃ©."
 
--- Target Labels
-Locales['target_lock'] = "Verrouiller le Véhicule"
-Locales['target_unlock'] = "Déverrouiller le Véhicule"
-Locales['target_give_key'] = "Donner les Clés du Véhicule"
+Locales['target_lock'] = "Verrouiller le VÃ©hicule"
+Locales['target_unlock'] = "DÃ©verrouiller le VÃ©hicule"
+Locales['target_give_key'] = "Donner les ClÃ©s du VÃ©hicule"
 
--- Key Interaction
-Locales['key_taken_from_vehicle'] = "Clé prise du véhicule."
-Locales['key_stolen'] = "La clé a été volée de votre véhicule !"
-Locales['key_given_to_player'] = "Clé donnée avec succès au joueur."
+Locales['key_taken_from_vehicle'] = "ClÃ© prise du vÃ©hicule."
+Locales['key_stolen'] = "La clÃ© a Ã©tÃ© volÃ©e de votre vÃ©hicule !"
+Locales['key_given_to_player'] = "ClÃ© donnÃ©e avec succÃ¨s au joueur."
 
--- General Feedback
-Locales['no_players_nearby'] = "Aucun joueur à proximité."
-Locales['player_has_no_keys'] = "Ce joueur n'a pas de clés pour ce véhicule."
-Locales['player_received_key'] = "Vous avez reçu les clés de ce véhicule."
-Locales['player_gave_key'] = "Vous avez donné les clés de ce véhicule."
+Locales['no_players_nearby'] = "Aucun joueur Ã  proximitÃ©."
+Locales['player_has_no_keys'] = "Ce joueur n'a pas de clÃ©s pour ce vÃ©hicule."
+Locales['player_received_key'] = "Vous avez reÃ§u les clÃ©s de ce vÃ©hicule."
+Locales['player_gave_key'] = "Vous avez donnÃ© les clÃ©s de ce vÃ©hicule."
 
--- Debugging (optional for debugging purposes in development)
-Locales['debug_vehicle_locked'] = "Véhicule verrouillé avec succès."
-Locales['debug_vehicle_unlocked'] = "Véhicule déverrouillé avec succès."
-
+Locales['debug_vehicle_locked'] = "VÃ©hicule verrouillÃ© avec succÃ¨s."
+Locales['debug_vehicle_unlocked'] = "VÃ©hicule dÃ©verrouillÃ© avec succÃ¨s."
